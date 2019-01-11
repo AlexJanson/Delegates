3682ab63d6cbb25b74f42938d7fcbd1cbdc9cdf3
AlexJanson <alex@hoofddorp.nl>
2019-01-11 13:37:14 +0100

    enemy spawner and click handler

---
+++ Delegates/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public GameObject prefab;
+	public Transform spawnHolder;
+
+    public delegate void EnemyKilled();
+    public event EnemyKilled enemyKilledEvent;
+    
+    void Start()
+    {
+        StartCoroutine(SpawnEnemy());
+    }
+
+    IEnumerator SpawnEnemy()
+    {
+        yield return new WaitForSeconds(1f);
+        prefab.transform.position = new Vector3(Random.RandomRange(-2f, 2f), 0, Random.RandomRange(-2f, 2f));
+        Instantiate(prefab, spawnHolder);
+        if(enemyKilledEvent != null) {
+            enemyKilledEvent();
+        }
+        yield return StartCoroutine(SpawnEnemy());
+    }
+}
